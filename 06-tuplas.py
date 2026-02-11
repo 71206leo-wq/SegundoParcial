478304ac836f01140b49a3fd189abf4d6c29f44c
71206leo-wq <71206leo@universidaddeleon.edu.mx>
2026-02-11 17:18:31 -0600

    segundoParcial

---
+++ 06-tuplas.py
@@ -0,0 +1,12 @@
+tupla = (1, 2, 3, 4, 5)
+print(type(tupla))
+print(tupla)
+print("el elemento de la tupla es:", tupla[2])
+for i in tupla:
+    print(i)
+    
+print("La cantidad de elementos de la tupla es:", len(tupla))
+print("La cantidad de veces que se repite el numero 2 es:", tupla.count(2))
+print("El indice del numero 3 es:", tupla.index(3))
+
+tupla[1]=30
